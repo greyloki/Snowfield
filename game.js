959374f89ad730247167f54f71eb162cf32fd2ea
Brian Foley <bffoley@gmail.com>
2011-12-30 14:43:20 -0500

    added in collision from .as

--- game.js
+++ game.js
@@ -1,5 +1,5 @@
-var width = 320,
-height = 500,
+var width = 800,
+height = 600,
 gLoop,
 c = document.getElementById('c'),
 ctx = c.getContext('2d'),
@@ -27,8 +27,10 @@ var Rect = function(x,y,w,h){
     this.y = y;
     this.w = w; //width
     this.h = h; //height
-    this.r = this.x+this.w; //right side
-    this.b = this.y+this.h; //bottom
+    this.width = w;
+    this.height = h;
+    this.r = x+w; //right side
+    this.b = y+h; //bottom
 }
 
 
@@ -48,42 +50,19 @@ var player = new (function(){
     that.X = 0;
     that.Y = 0;
     that.actions = [];
-    that.TL = [0,0];
-    that.TM = [0,0];
-    that.TR = [0,0];
-    that.CL = [0,0];
-    that.CR = [0,0];
-    that.BL = [0,0];
-    that.BM = [0,0];
-    that.BR = [0,0];
+
 
     that.setPosition = function(x, y){  //sets location
         that.X = x;
         that.Y = y;
     }
     
-    that.setPoints = function(){
-        that.TL = [that.X,                  that.Y];
-        that.TM = [that.X+(that.width/2),   that.Y];
-        that.TR = [that.X+that.width,       that.Y];
-        
-        that.CL = [that.X,                  that.Y+(that.height/2)];
-        that.CR = [that.X+that.width,       that.Y+(that.height/2)];
-        
-        that.BL = [that.X,                  that.Y+that.height];
-        that.BM = [that.X+(that.width/2),   that.Y+that.height];
-        that.BR = [that.X+that.width,       that.Y+that.height];
-        console.log(that.TL+" : "+that.TM+" : "+that.TR);
-        console.log(that.CL+" : 000,000 : "+that.CR);
-        console.log(that.BL+" : "+that.BM+" : "+that.BR);
-        console.log("\n");
-        
-    }
-
     that.isJumping = false;
     that.isFalling = false;
     that.isLeft = false;
     that.isRight = false;
+    that.isUp = false;
+    that.isDown = false;
     that.jumpSpeed = 0;
     that.jumpMax = 15;
     that.fallSpeed = 0;
@@ -110,6 +89,22 @@ var player = new (function(){
                 
                 case "stopRight":
                     that.isRight = false;  
+                    break;
+                
+                case "moveUp":
+                    that.isUp = true;
+                    break;
+                
+                case "moveDown":
+                    that.isDown = true;  
+                    break;
+                
+                case "stopUp":
+                    that.isUp = false;
+                    break;
+                
+                case "stopDown":
+                    that.isDown = false;  
                     break;
                 
                 case "collide":
@@ -139,7 +134,8 @@ var player = new (function(){
             }//end action switch
             
         }//end loop
-            
+
+/*            
         if (that.isJumping){
             that.Y -= that.jumpSpeed;
             that.jumpSpeed--;  
@@ -159,23 +155,47 @@ var player = new (function(){
                 that.fallSpeed = 0;
             }              
         }
-        
+*/        
         if (that.isLeft){
-            if( (that.X-that.moveSpeed) > 0  ){ //if this not land us left of the wall
-                //if( CheckNewCollision(new Point(), new Rect()  ))
-                that.X -= that.moveSpeed;
-            }else{
-                that.X = 0;
-            }            
+            that.X -= CheckMove(
+                new Point(that.X-that.moveSpeed, that.Y), 
+		new Point(that.X-that.moveSpeed, that.Y+(that.height/2) ), 
+		new Point(that.X-that.moveSpeed,(that.Y+that.height)),
+		"l",that.moveSpeed 
+	    );
         }
         
         if (that.isRight){
-            if( (that.X+that.moveSpeed) < (width-that.width)  ){ //if this not land us right of the wall
-                that.X += that.moveSpeed;
-            }else{
-                that.X = width-that.width;
-            }              
+
+            that.X += CheckMove(
+                new Point(that.X+that.width+that.moveSpeed, that.Y), 
+                new Point(that.X+that.width+that.moveSpeed, that.Y+(that.height/2) ), 
+		new Point(that.X+that.width+that.moveSpeed,(that.Y+that.height)),  
+		"r",that.moveSpeed
+	    );            
+            
+            
         }
+        
+        if (that.isUp){
+            that.Y -= CheckMove(
+                new Point(that.X, that.Y-that.moveSpeed), 
+		new Point(that.X+(that.width/2), that.Y-that.moveSpeed), 
+		new Point(that.X+that.width, that.Y-that.moveSpeed),
+		"u",that.moveSpeed
+            );   
+        }        
+        
+        if (that.isDown){
+            that.Y +=CheckMove(
+                new Point(that.X, that.Y+that.height+that.moveSpeed), 
+                new Point(that.X+(that.width/2), that.y+that.height+that.moveSpeed), 
+		new Point(that.X+that.width, that.y+that.height+that.moveSpeed),
+		"d",that.moveSpeed  
+	    );    
+        }        
+        
+        
         
         if(that.isJumping || that.isFalling){
             that.image.src = "greenbox.png";
@@ -202,10 +222,7 @@ var player = new (function(){
     }
 })();   //end player
 
-player.setPosition(~~((width-player.width)/2), ~~((height - player.height)));   //starting position TODO: Move this
-
-qwer = new Point(5,6);
-console.log(qwer);
+player.setPosition(~~((width-player.width)/2), ~~((height - player.height)/2));   //starting position TODO: Move this
 
 
 //--------------------------------------------------
@@ -222,7 +239,9 @@ document.onkeydown = function(e){
     }else if(actualkey=='D'){
         player.actions.push('moveRight');
     }else if(actualkey=='W'){
-        player.actions.push("Jump");
+        player.actions.push("moveUp");    //Jump
+    }else if(actualkey=='S'){
+        player.actions.push("moveDown");
     }else if(actualkey=='Q'){
         player.setPoints();
     }else if(actualkey=='E'){
@@ -239,6 +258,10 @@ document.onkeyup = function(e){
         player.actions.push('stopLeft');
     }else if(actualkey=='D'){
         player.actions.push('stopRight');
+    }else if(actualkey=='W'){
+        player.actions.push('stopUp');
+    }else if(actualkey=='S'){
+        player.actions.push('stopDown');
     }
 }
 
@@ -255,6 +278,11 @@ platformHeight = 20;
 
 var Platform = function(x, y, type){
     var that=this;
+    this.width = 70;
+    this.height=20;
+    
+    that.width = 70;
+    that.height=20;
 
     that.firstColor = '#FF8C00';
     that.secondColor = '#EEEE00';
@@ -272,11 +300,11 @@ var Platform = function(x, y, type){
 
     that.draw = function(){
         ctx.fillStyle = 'rgba(255, 255, 255, 1)';
-        var gradient = ctx.createRadialGradient(that.x + (platformWidth/2), that.y + (platformHeight/2), 5, that.x + (platformWidth/2), that.y + (platformHeight/2), 45);
+        var gradient = ctx.createRadialGradient(that.x + (that.width/2), that.y + (that.height/2), 5, that.x + (that.width/2), that.y + (that.height/2), 45);
         gradient.addColorStop(0, that.firstColor);
         gradient.addColorStop(1, that.secondColor);
         ctx.fillStyle = gradient;
-        ctx.fillRect(that.x, that.y, platformWidth, platformHeight);
+        ctx.fillRect(that.x, that.y, that.width, that.height);
     };
 
     return that;
@@ -293,34 +321,97 @@ var generatePlatforms = function(){
         }
     }
  */
-    platforms[0]=new Platform(0,height-platformHeight-5,0);
+    platforms[0]=new Platform(20,height-platformHeight-5,0);
 }();
 
 //--------------------------------------------------
 // MISC
 //--------------------------------------------------
 
-var checkCollision = function(){
-    platforms.forEach(function(e,  ind){
-    if (
-        //(player.isFalling) &&
-        (player.X < e.x + platformWidth) &&
-        (player.X + player.width > e.x) &&
-        (player.Y + player.height > e.y) &&
-        (player.Y + player.height < e.y + platformHeight)
-    ) {
-        player.actions.push("collide");
-        //e.onCollide();
+    var CheckMove = function(p1,p2,p3,dir,speed){
+        var tempObj;
+        var aHit = false;
+        var hitP = new Point(0,0);
+        var hitRect = new Rect(0,0,0,0);
+			
+            platforms.forEach(function(e,  ind){
+		tempObj = new Rect(e.x,e.y,e.width,e.height);
+                
+                if( hitTestPoint(p1.x,p1.y,tempObj)   ){
+		    console.log("Hit P1");
+                    aHit=true;
+                    hitP = p1;
+                    
+                    //xyz = FindAcceptable(tempObj,p1,dir,speed);
+		}else if( hitTestPoint(p2.x,p2.y,tempObj)  ){
+                    console.log("Hit P2");
+                    aHit=true;
+                    hitP = p2;
+                    
+                }else if(hitTestPoint(p3.x,p3.y,tempObj)){
+                    console.log("Hit P3");
+		    aHit=true;
+                    hitP = p3;
+                    
+		}
+                    
+	    });
+            //console.log("MoveIt: ",MoveIt);
+        if(!aHit){return speed;}
+        else{
+            return FindAcceptable(tempObj,hitP,dir,speed);
+        }
     }
-})
-}
 
-var CheckNewCollision = function(p,r){
-    return (
-            (r.x <= p.x) && (p.x <= (r.x + r.w ))
-            )
-    && ((r.y <= p.y) && (p.y <= (r.y + r.h)));
-}
+    var hitTestPoint = function(px,py,Re){
+        if ( ( (px >= Re.x) && (px <= Re.x+Re.width) ) && ( (py >= Re.y) &&  (py <= Re.y+Re.height) )){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+    var FindAcceptable = function (Obj,p1,dir,speed){
+			
+        var NP;
+    	switch(dir){
+		case "u":
+		    NP = new Point(0,-1);
+		    break;
+		
+                case "d":
+                    NP = new Point(0,1);
+                    break;
+				
+                case "l":
+		    NP = new Point(-1,0);
+		    break;
+		
+                case "r":
+		    NP = new Point(1,0);
+		    break;
+		
+                default:
+		    NP = new Point(0,0);
+		    break;
+				
+	}
+        for(var q=speed;q>=0;q--){
+	    if (!hitTestPoint(p1.x,p1.y,Obj)){
+                console.log(Obj.x,p1.x,(Obj.x+Obj.width));
+                console.log(Obj.y,p1.y,(Obj.y+Obj.height));
+                console.log("");
+                //console.log("returning ",q);
+		return q;					
+	    }else{
+                console.log("Removing");
+		p1.X -= NP.x;
+		p1.Y -= NP.y;
+	    }							
+	}
+    return 0;
+    }
+
 
 
 var GameLoop = function(){
@@ -329,8 +420,6 @@ var GameLoop = function(){
     platforms.forEach(function(platform){   //draw all the platforms
         platform.draw();
     });
-
-    checkCollision();   //check if player hits platforms.
     
     player.draw();
     gLoop = setTimeout(GameLoop, 1000 / 50);
